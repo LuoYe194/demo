0941add53b0b6bcbd3cdf080b5acb779f66cf76b
LY <LY194@hotmail.com>
2020-02-20 22:06:19 +0800

    test

--- demo.py
+++ demo.py
@@ -1,10 +1,18 @@
 print("demo")
 print(".")
 
+
 def min(a, b):
     if a < b:
         return a
     return b
 
 print(min(1, 2))
+
+def max(a, b):
+    if a > b:
+        return a
+    return b
+
+print(max(1, 2))
 
